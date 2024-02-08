efa972db5c033d5596b5e4c59b05ac5879131b68
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-02-08 15:21:06 -0300

    renderizar cards na tela

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LivroService {
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  buscar(valorDigitado: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', valorDigitado);
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após map', resultado))
+    );
+  }
 }
